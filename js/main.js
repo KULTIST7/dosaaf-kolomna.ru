d5d84add6db223fae83059499dd41d9fe50360c8
KULTIST7 <kultist77@gmail.com>
2024-07-02 14:18:22 +0500

    inputmask added

--- js/main.js
+++ js/main.js
@@ -28,6 +28,16 @@ $(document).ready(() => {
 			}, 500, 'swing');
 		}
 	);
+
+	$('input[type=tel]').inputmask({
+		mask: '+7 (*{1}99) 999-99-99',
+		placeholder: "+7 (___) ___-__-__",
+		definitions: {
+			'*': {
+				validator: "[0-6,9]"
+			}
+		}
+	});
 });
 
 /******/ (() => {
